72664b182e5f153224a872ece79450da8272d89e
Rodrigo Silva <rod.silva1988@gmail.com>
2020-06-25 20:24:39 -0400

    Fix url parameter and separator (windows platform)

--- cloud-academy-dl.py
+++ cloud-academy-dl.py
@@ -4,7 +4,7 @@ Downloads the video lectures for the given CloudAcademy course.
 
 Usage:
     cloud-academy-dl --help
-    cloud-academy-dl [--res=<resolution>] [--cookie=<txt_file>] [--out=<output_dir>] <url>
+    cloud-academy-dl <url> [--cookie=<txt_file>] [--out=<output_dir>] [--res=<resolution>]
 
 Options:
     --help              Shows this screen.
@@ -13,8 +13,8 @@ Options:
                         720, and 1080 [default: 1080].
     --out=<output_dir>  The directory where the videos are saved
                         [default: courses].
-    --cookie=<txt_file>  Text file with all Request header from CloudAcademy
-    url
+    --cookie=<txt_file> Text file with all Request header from CloudAcademy
+    url                 On windows platform the url must be in double quotes
 '''
 
 import json
@@ -47,44 +47,48 @@ def fetch_videos(course_url, cookies):
 def get_course_contents(course_url, cookies, output_dir, video_res):
     folder_sep = get_os_platform()
     data = fetch_videos(course_url, cookies)
-    course_title = data['course']['includedIn'][0]['title']
-    module = data['course']['entity']['title']
-    print("Course : " + course_title)
-    print("Module : " + module)
-    resolution = video_res+'p'
-    url_spliter = str(course_url).rsplit('/', 2)
+    try:
+        course_title = data['course']['includedIn'][0]['title']
+        module = data['course']['entity']['title']
+        print("Course : " + course_title)
+        print("Module : " + module)
+        resolution = video_res+'p'
+        url_spliter = str(course_url).rsplit('/', 2)
 
-    for index, modules in enumerate(data['course']['entity']['steps']):
-        url = url_spliter[0] + '/' + data['course']['entity']['steps'][index]['slug'] + '/' + url_spliter[2]
-        content = fetch_videos(url, cookies)
+        for index, modules in enumerate(data['course']['entity']['steps']):
+            url = url_spliter[0] + '/' + data['course']['entity']['steps'][index]['slug'] + '/' + url_spliter[2]
+            content = fetch_videos(url, cookies)
 
-        for item in content['course']['stepMap']:
-            if index < 9:
-                prefix = '0'+str(index+1)+'_'
-            else:
-                prefix = str(index+1)+'_'
-            title_video =  re.findall(r'([a-zA-Z0-9 -]*)', content['course']['stepMap'][item]['data']['title'])[0]
-            subs_url = content['course']['stepMap'][item]['data']['player']['subtitles'][0]['url']
+            for item in content['course']['stepMap']:
+                if index < 9:
+                    prefix = '0'+str(index+1)+'_'
+                else:
+                    prefix = str(index+1)+'_'
+                title_video =  re.findall(r'([a-zA-Z0-9 -]*)', content['course']['stepMap'][item]['data']['title'])[0]
+                subs_url = content['course']['stepMap'][item]['data']['player']['subtitles'][0]['url']
 
-            for sources in content['course']['stepMap'][item]['data']['player']['sources']:
-                if sources['quality'] == resolution and sources['type'] == 'video\u002Fmp4':
-                    video_url = sources['src']
-                    path = output_dir +folder_sep + course_title + folder_sep + module + folder_sep +  prefix+title_video + folder_sep
-                    video_file = path + title_video + ".mp4"
-                    subs_name_file = path + title_video + ".vtt"
-                    os.makedirs(path, exist_ok=True)
-                    download_file(video_url, video_file)
-                    download_file(subs_url, subs_name_file)
+                for sources in content['course']['stepMap'][item]['data']['player']['sources']:
+                    if sources['quality'] == resolution and sources['type'] == 'video\u002Fmp4':
+                        video_url = sources['src']
+                        path = output_dir +folder_sep + course_title + folder_sep + module + folder_sep +  prefix+title_video + folder_sep
+                        video_file = path + title_video + ".mp4"
+                        subs_name_file = path + title_video + ".vtt"
+                        os.makedirs(path, exist_ok=True)
+                        download_file(video_url, video_file)
+                        download_file(subs_url, subs_name_file)
+    except KeyError as error:
+        print(error)
 
 
 
-def download_file(url, dest_filaneme,):
+def download_file(url, dest_filaneme):
+    splitter = get_os_platform()
     resp = requests.get(url, stream=True)
 
     total_size = int(resp.headers.get('content-length'))
 
-    text = 'Downloading {file:<60}'.format(file=dest_filaneme.rsplit('/',1)[1])
-    with tqdm.tqdm(total=total_size, unit_scale=True, ncols=100000, desc=text, bar_format="{desc:<11} {percentage:3.0f}%s %s|%s{bar:100}{r_bar}" % ('%',Fore.LIGHTGREEN_EX, Fore.LIGHTGREEN_EX)) as progress:
+    text = 'Downloading {file:<60}'.format(file=dest_filaneme.rsplit(splitter,1)[1])
+    with tqdm.tqdm(total=total_size, dynamic_ncols=True, unit_scale=True, desc=text, bar_format="{desc:<11} {percentage:3.0f}%s %s|%s{bar:50}{r_bar}" % ('%',Fore.LIGHTGREEN_EX, Fore.LIGHTGREEN_EX)) as progress:
         with open(dest_filaneme, 'wb') as f:
             for chunk in resp.iter_content(chunk_size=4096):
                 progress.update(len(chunk))
